5107494781290df2f0c5e6f8530b6e7b479c63dd
KimJaeMin <tuiab25906370@gmail.com>
2022-10-17 14:54:06 +0900

    2022-10-17_5Class

---
+++ grade/GradeEvaluation.java
@@ -0,0 +1,7 @@
+package grade;
+
+public interface GradeEvaluation {
+    public String getGrade(int point);
+    //점수가 있으면 그 점수에 따른 학점을
+    //문자열로 반환해준다.
+}
---
+++ utils/Define.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Define {
+    public static final int KOREAN = 1001;
+    public static final int MATH = 2001;
+
+    public static final int AB_TYPE = 0;    // 일반 과목
+    public static final int SAB_TYPE = 1;   // 필수 과목
+    public static final int PF_TYPE = 2;    // pass, fail
+
+}
