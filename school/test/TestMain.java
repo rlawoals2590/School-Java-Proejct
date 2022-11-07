e228e3346b2c2e691e6963d75f21b7edb923516b
rlawoals2590 <tuiab25906370@gmail.com>
2022-11-07 16:20:00 +0900

    2022-11-07_6,7Class

--- school/test/TestMain.java
+++ school/test/TestMain.java
@@ -1,6 +1,8 @@
 package school.test;
 
 import school.School;
+import school.Score;
+import school.Student;
 import school.Subject;
 import school.report.GenerateGradeReport;
 import utils.Define;
@@ -14,6 +16,7 @@ public class TestMain {
     public static void main(String[] args){
         TestMain test = new TestMain();
         test.createSubject();
+        test.createStudent();
 
     }
 
@@ -23,6 +26,42 @@ public class TestMain {
 
         school.addSubject(korean);
         school.addSubject(math);
+    }
+
+    public void createStudent(){
+        Student student1 = new Student(221213, "안성원", korean);
+        Student student2 = new Student(221518, "오태훈", math);
+        Student student3 = new Student(221230, "이동호", korean);
+        Student student4 = new Student(221255, "조성욱", korean);
+        Student student5 = new Student(221590, "최태평", math);
+
+        school.addStudent(student1);
+        school.addStudent(student2);
+        school.addStudent(student3);
+        school.addStudent(student4);
+        school.addStudent(student5);
+
+        korean.register(student1);
+        korean.register(student2);
+        korean.register(student3);
+        korean.register(student4);
+        korean.register(student5);
+
+        math.register(student1);
+        math.register(student2);
+        math.register(student3);
+        math.register(student4);
+        math.register(student5);
+
+        Score score;
+        score = new Score(student1.getStudentId(), korean, 95);
+        student1.addSubjectScore(score);
+        addScoreForStudent(student1, korean, 95);
+
+    }
+
+    public void addScoreForStudent(Student student, Subject subject, int point){
+        Score score = new Score(student.getStudentId(), subject, point);
     }
 
 }
